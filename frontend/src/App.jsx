b6d097a67a5993f7c2ffc9494b2aa67b2abdbbee
himanshu6387 <mishrahimanshuu01@gmail.com>
2025-09-26 18:38:52 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ import AdminDashboard from './pages/AdminDashboard';
 import {Toaster} from 'react-hot-toast'
 import Gallery from './components/Gallery/Gallery';
 import About from './components/About/About';
-import ScrollToTop from './components/common/ScrollToTop';
+// import ScrollToTop from './components/common/ScrollToTop';
 
 function App() {
   return (
@@ -33,7 +33,7 @@ function App() {
           </Routes>
         </main>
         <Footer />
-        <ScrollToTop/>
+        {/* <ScrollToTop/> */}
         <Toaster/>
       </div>
     </Router>
