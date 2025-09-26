ef2ae30475b165eb06fe4d2f9f2f2701acabfc6a
himanshu6387 <mishrahimanshuu01@gmail.com>
2025-09-26 15:05:16 +0530

    updated code

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        mongoose.connect('MONGO_URI=mongodb+srv://himanshu01:Iammongodb%407@cluster0.jz9puwy.mongodb.net/CollegeERP?retryWrites=true&w=majority&appName=Cluster0')
+        mongoose.connect('mongodb+srv://himanshu01:Iammongodb%407@cluster0.jz9puwy.mongodb.net/CollegeERP?retryWrites=true&w=majority&appName=Cluster0')
         console.log('MongoDB Connected Successfully..')
     } catch (error) {
         console.log(error)
